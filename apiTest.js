26d717c9470d9d4104c1f30641ceb37d143ed448
jusbuckingham <jus.buckingham@gmail.com>
2021-12-29 07:26:50 -0800

    adding axios and react router dom

---
+++ apiTest.js
@@ -0,0 +1,11 @@
+const axios = require("axios");
+
+axios.get('')
+        .then((response) => {
+            console.log(response.data)
+            
+        })
+        .catch((error) => {
+            console.log('error hitting api', error);
+        });
+  
