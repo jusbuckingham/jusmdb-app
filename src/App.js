d81f4c0067465a5b848256178577969a816dd809
jusbuckingham <jus.buckingham@gmail.com>
2021-12-29 19:40:35 -0800

    building out components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Components
+import Home from "./components/Home"
+import Top from "./components/Top"
+import Actors from "./components/Actors";
+import Movies from "./components/Movies";
+import Tvshows from "./components/Tvshows";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/top" element={<Top />} />
+            <Route path="/actors" element={<Actors />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/tvshows" element={<Tvshows />} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
